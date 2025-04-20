3eec64eff649af4c8ae74bfa1bc6d49e6c383c29
Arianna Tobias <ariannatobias@ou.edu>
2025-04-20 16:28:30 -0500

    working on frontend setup for the app. want very descriptive and a modern look

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import DVoteApp from './DVoteApp';
+
+function App() {
+  return (
+    <div className="App">
+      <DVoteApp />
+    </div>
+  );
+}
+
+export default App;
